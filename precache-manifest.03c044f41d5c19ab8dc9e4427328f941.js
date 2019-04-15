42ba6521124f4aa2c8c598d0c74ad316626e3a25
Taylor Richards <taylor.rminda@gmail.com>
2019-04-14 20:33:29 -0500

    Updates

---
+++ precache-manifest.03c044f41d5c19ab8dc9e4427328f941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0367e36113d279641c6",
+    "url": "/gbooks-search/static/js/runtime~main.23b6aefd.js"
+  },
+  {
+    "revision": "58f520fcb80470de574b",
+    "url": "/gbooks-search/static/js/main.c3af6ac8.chunk.js"
+  },
+  {
+    "revision": "f23d1f7e995ba1439ce1",
+    "url": "/gbooks-search/static/js/2.310a757e.chunk.js"
+  },
+  {
+    "revision": "58f520fcb80470de574b",
+    "url": "/gbooks-search/static/css/main.5098186c.chunk.css"
+  },
+  {
+    "revision": "e4229d58980045e13ce50bd9dbc37d38",
+    "url": "/gbooks-search/index.html"
+  }
+];
